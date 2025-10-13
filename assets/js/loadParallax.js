1449a56746a89c5c7e1b6be79575b19edd0b96b7
tony <toni.felixx@gmail.com>
2025-10-13 16:56:47 -0300

    reduz arquivoz css | tira svg inline

---
+++ assets/js/loadParallax.js
@@ -0,0 +1,6 @@
+if (window.innerWidth > 768) {
+  const script = document.createElement('script');
+  script.src = 'assets/js/heroParallax.js';
+  script.async = true;
+  document.body.appendChild(script);
+}
